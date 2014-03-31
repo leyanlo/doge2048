124ae61111da24dfe24cc54e345278a54b5cb74a
Leyan Lo <leyanlo@gmail.com>
2014-03-30 18:46:11 -0700

    Highfive-ified doge2048

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# Doge2048
-Just a couple of minor tweakts to Gabriele Cirulli's code base [here](https://github.com/gabrielecirulli/2048)
+# Highfive 2048
+Just a couple of minor tweakts to Jason LaFerrera's code base [here](https://github.com/laferrera/doge2048)
 
-Made just for fun. [Play it here!](http://doge2048.com)
+Made just for fun. [Play it here!](http://leyanlo.github.io/highfive2048/)
 
 
 ## License
-Doge2048 is licensed under the [MIT license.](https://github.com/laferrera/doge2048/blob/master/LICENSE.txt)
-
+Highfive 2048 is licensed under the [MIT license.](https://github.com/laferrera/doge2048/blob/master/LICENSE.txt)
--- js/html_actuator.js
+++ js/html_actuator.js
@@ -5,19 +5,12 @@ function HTMLActuator() {
   this.messageContainer = document.querySelector(".game-message");
   this.info             = document.querySelector(".info");  
   this.dogeSays = document.querySelector(".doge-says");
-  this.adSpace = document.querySelector(".shout-out");
 
   this.score = 0;
 }
 
-var dogeSayings = ['such good', 'so amaze', 'many points', 'very unstoppable', 'great jorb', 'such playing', 'very good', 'points', 'very gaming', 'such player', 'concern' ,'bewildered',
-'many game', 'so good', 'very scores', 'so scoring', 'so hot right now', 'such playing', 'such matching', 'so matched', 'very matched', 'very neat' ,'such natural',]
-
-var ads = [
-
-  '<a href="https://itunes.apple.com/us/app/snack-compass/id646138186?mt=8&ign-mpt=uo%3D4" target="_blank">Like Pizza?</a>',
-  '<a href="http://maxhash.com/doge" target="_blank">Check out everything hashtagged Doge!</a>',
-]
+var dogeSayings = ['such engineering', 'so startup', 'many entrepreneur', 'very video conferencing', 'such face to face', 'very conference room', 'very meeting', 'such highfive',
+  'many prototypes', 'so light ring', 'very marketing', 'so h264', 'so video pipeline', 'such dogfooding','such telemetry', 'so echo cancellation', 'very multicast', 'very mexican standoff', 'such chinese firedrill'];
 
 HTMLActuator.prototype.actuate = function (grid, metadata) {
   var self = this;
@@ -129,7 +122,10 @@ HTMLActuator.prototype.updateScore = function (score) {
     addition.classList.add("score-addition");
     addition.textContent = "+" + difference;
     this.scoreContainer.appendChild(addition);
-    
+
+    var boratHighfive = new Audio("raw/borat-highfive-short.mp3"); // buffers automatically when created
+    boratHighfive.play();
+
     var message = dogeSayings[Math.floor(Math.random() * dogeSayings.length)]
     var messageElement = document.createElement("p");
     messageElement.textContent = message
@@ -139,10 +135,6 @@ HTMLActuator.prototype.updateScore = function (score) {
     var styleString = left + top + color
     messageElement.setAttribute('style', styleString);
     this.dogeSays.appendChild(messageElement);
-    if (difference > 4) {
-     this.adSpace.innerHTML = ads[Math.floor(Math.random() * ads.length)]
-    }
-    
   }
 };
 
